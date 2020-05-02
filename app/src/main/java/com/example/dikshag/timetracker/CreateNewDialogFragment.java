fce0556abe188e6987fb8fe53446c77db004ec3a
Diksha Gohlyan <gr8bigdg@gmail.com>
2020-05-02 12:39:40 -0700

    Create a dialog when user clicks on + This resolves issue #1

---
+++ app/src/main/java/com/example/dikshag/timetracker/CreateNewDialogFragment.java
@@ -0,0 +1,16 @@
+package com.example.dikshag.timetracker;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CreateNewDialogFragment extends DialogFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout to use as dialog or embedded fragment
+        return inflater.inflate(R.layout.create_new_dialog, container, false);
+    }
+}
--- app/src/main/java/com/example/dikshag/timetracker/MainActivity.java
+++ app/src/main/java/com/example/dikshag/timetracker/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.dikshag.timetracker;
 
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,18 +17,27 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
     private TimeTrackerNode rootTimeTrackerNode;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.time_tracker_nodes);
+        button = findViewById(R.id.add_button);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         init();
         // specify an adapter (see also next example)
         mAdapter = new TimeTrackerAdapter(getTimeTrackerRecords());
         recyclerView.setAdapter(mAdapter);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment createNewFragment = new CreateNewDialogFragment();
+                createNewFragment.show(getSupportFragmentManager(), "createNewDialog");
+            }
+        });
 
     }
 
